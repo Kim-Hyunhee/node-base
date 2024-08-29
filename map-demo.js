4cd9e04c3b4246452d62335910c87d4f39d123d6
Kim-Hyunhee <tta_bi@naver.com>
2024-08-29 10:52:15 +0900

    map 테스트

---
+++ map-demo.js
@@ -0,0 +1,9 @@
+let db = new Map();
+db.set(1, "NoteBook"); // 키로 벨류를 찾을 수 있는 한 쌍을 저장
+db.set(2, "Cup");
+db.set(3, "Chair");
+
+console.log(db);
+console.log(db.get(1));
+console.log(db.get(2));
+console.log(db.get(3));
