52717e9ff54a8b14bc53ef259575c647feeb008b
Kim-Hyunhee <tta_bi@naver.com>
2024-09-03 09:37:02 +0900

    예외 고도화

--- demo-api/youtuber-demo.js
+++ demo-api/youtuber-demo.js
@@ -32,12 +32,17 @@ db.set(id++, youtuber3);
 
 app.get("/youtubers", (req, res) => {
   var youtubers = {};
-  // value: 데이터, key: 인덱스
-  db.forEach((value, key) => {
-    youtubers[key] = value;
-  });
 
-  res.json(youtubers);
+  if (db.size !== 0) {
+    // value: 데이터, key: 인덱스
+    db.forEach((value, key) => {
+      youtubers[key] = value;
+    });
+
+    res.json(youtubers);
+  } else {
+    res.status(404).json({ message: "조회할 유튜버가 없습니다." });
+  }
 });
 
 app.get("/youtubers/:id", function (req, res) {
@@ -46,7 +51,7 @@ app.get("/youtubers/:id", function (req, res) {
 
   const youtuber = db.get(id);
   if (db.get(id) == undefined) {
-    res.json({ message: "유튜버 정보를 찾을 수 없습니다." });
+    res.status(404).json({ message: "유튜버 정보를 찾을 수 없습니다." });
   } else {
     res.json(youtuber);
   }
@@ -77,22 +82,21 @@ app.delete("/youtubers/:id", function (req, res) {
       message: `${channelTitle}님, 아쉽지만 다음에 또 뵙겠습니다.`,
     });
   } else {
-    res.json({ message: `요청하신 ${id}번은 없는 유튜버입니다.` });
+    res.status(404).json({ message: `요청하신 ${id}번은 없는 유튜버입니다.` });
   }
 });
 
 app.delete("/youtubers", function (req, res) {
   // db에 값이 1개 이상이면, 전체 삭제
-  var msg = "";
   if (db.size >= 1) {
     db.clear();
-    msg = "전체 유튜버가 삭제되었습니다.";
+
+    res.json({ message: "전체 유튜버가 삭제되었습니다." });
   }
   // 값이 없으면, "삭제할 유튜버가 없습니다."
   else {
-    msg = "삭제할 유튜버가 없습니다.";
+    res.status(404).json({ message: "삭제할 유튜버가 없습니다." });
   }
-  res.json({ message: msg });
 });
 
 app.put("/youtubers/:id", function (req, res) {
@@ -102,7 +106,7 @@ app.put("/youtubers/:id", function (req, res) {
   var youtuber = db.get(id);
   var oldTitle = youtuber.channelTitle;
   if (youtuber == undefined) {
-    res.json({ message: `요청하신 ${id}번은 없는 유튜버입니다.` });
+    res.status(404).json({ message: `요청하신 ${id}번은 없는 유튜버입니다.` });
   } else {
     var newTitle = req.body.channelTitle;
 
